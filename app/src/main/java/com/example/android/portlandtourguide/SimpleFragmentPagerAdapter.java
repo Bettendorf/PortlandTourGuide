f519ba699536461b42f2bc78f143f460b7ed6422
Scotland Bettendorf <scotbettendorf@gmail.com>
2018-07-10 01:27:58 -0700

    Before fragment name updates.

--- app/src/main/java/com/example/android/portlandtourguide/SimpleFragmentPagerAdapter.java
+++ app/src/main/java/com/example/android/portlandtourguide/SimpleFragmentPagerAdapter.java
@@ -1,5 +1,7 @@
 package com.example.android.portlandtourguide;
 
+import android.content.Context;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -9,8 +11,12 @@ import android.support.v4.app.FragmentPagerAdapter;
  */
 public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
 
-    public SimpleFragmentPagerAdapter(FragmentManager fm) {
+    /** Context of the app */
+    private Context mContext;
+
+    public SimpleFragmentPagerAdapter(Context context, FragmentManager fm) {
         super(fm);
+        mContext = context;
     }
 
     @Override
@@ -26,8 +32,25 @@ public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
         }
     }
 
+    /**
+    * Return the number of pages.
+     */
     @Override
     public int getCount() {
         return 4;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return mContext.getString(R.string.category_one);
+        } else if (position == 1) {
+            return mContext.getString(R.string.category_two);
+        } else if (position == 2) {
+            return mContext.getString(R.string.category_three);
+        } else {
+            return mContext.getString(R.string.category_four);
+        }
     }
 }
