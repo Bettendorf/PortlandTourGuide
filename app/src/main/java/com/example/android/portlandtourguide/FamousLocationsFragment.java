882e5e70acc08b8b7a14ef298891fd3bb3cd59ee
Scotland Bettendorf <scotbettendorf@gmail.com>
2018-07-13 14:16:25 -0700

    Updated the strings.xml file to contain the information for each guide location.

--- app/src/main/java/com/example/android/portlandtourguide/FamousLocationsFragment.java
+++ app/src/main/java/com/example/android/portlandtourguide/FamousLocationsFragment.java
@@ -5,6 +5,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * Fragment that displays "Famous Locations".
@@ -13,6 +16,17 @@ public class FamousLocationsFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.locations_list, container, false);
+        View rootView = inflater.inflate(R.layout.locations_list, container, false);
+
+        ArrayList<GuideLocation> guideLocationsList = new ArrayList<>();
+        guideLocationsList.add(new GuideLocation(getString(R.string.famous_location_1_name), getString(R.string.famous_location_1_address)));
+        guideLocationsList.add(new GuideLocation(getString(R.string.famous_location_2_name), getString(R.string.famous_location_2_address)));
+
+        GuideLocationAdapter itemsAdapter = new GuideLocationAdapter(getContext(), guideLocationsList);
+
+        ListView listView = rootView.findViewById(R.id.locations_list_view);
+        listView.setAdapter(itemsAdapter);
+
+        return rootView;
     }
 }
